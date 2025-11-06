4728b1074e5b48de301e0c8797856f0ea71e41e2
rawadkh0 <rawadkh0@gmail.com>
2025-11-06 21:09:18 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,3 +1,5 @@
+import math
+
 def find_max_number(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
     return num1
@@ -10,7 +12,6 @@ def find_max_number(num1, num2, num3):
 def find_mean(num1, num2, num3):
     return (num1 + num2 + num3) / 3
 
-import math
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     variance = ((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2) / 3
